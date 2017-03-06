99fecc0b919ef15cd9612de6ea8fff3bdc38a569
Rene-Pier Deshaies Gélinas <rene-pierdg@jomedia.local>
2017-03-06 14:03:47 -0500

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # fuse-box-process-plugin
-A Plugin for fuse-box that gives you the ability to run any tasks (npm, tsc, etc.) after fuse-box bundled your code
+A Plugin for fuse-box that gives you the ability to 
+ run any tasks (npm, tsc, etc.) after fuse-box bundled your code
+
+
+## How to use 
+```
+let fuse = new FuseBox({
+    homeDir: "src/",
+    sourcemaps: true,
+    outFile: "./build/out.js",
+    plugins: [
+        ProcessPlugin({
+            process: [{
+                    processName: 'npm',
+                    processArgs: ['run', 'lint'],
+                    verbose: true,
+                },
+                {
+                    processName: 'npm',
+                    processArgs: ['run', 'server'],
+                    verbose: false,
+                },
+            ]
+        }),
+    ]
+});
+```
+
+## Microsoft Windows...
+For Microsoft windows, you might need to do something like this to run commands : 
+
+```
+const isWindows: boolean = /^win/.test(process.platform);
+
+let fuse = new FuseBox({
+    homeDir: "src/",
+    sourcemaps: true,
+    outFile: "./build/out.js",
+    plugins: [
+        ProcessPlugin({
+            process: [{
+                    processName: isWindows ? 'npm.cmd' : 'npm',
+                    processArgs: ['run', 'lint'],
+                    verbose: true,
+                },
+                {
+                    processName: isWindows ? 'npm.cmd' : 'npm',
+                    processArgs: ['run', 'server'],
+                    verbose: false,
+                },
+            ]
+        }),
+    ]
+});
+```
---
+++ src/plugin.ts
@@ -0,0 +1,73 @@
+const spawn: any = require('child_process').spawn;
+
+interface IDictionary<T> {
+    [key: string]: T;
+}
+
+interface IProcessDetail {
+    processName: string;
+    processArgs: Array<string>;
+    verbose: boolean;
+}
+
+interface IProcess {
+    process: any;
+    detail: IProcessDetail;
+}
+
+type IProcessHash = IDictionary<IProcess>;
+
+interface IOptions {
+    process: Array<IProcessDetail>;
+}
+
+class ProcessPluginClass {
+    test: RegExp;
+    opts: IOptions;
+    processList: IProcessHash = {};
+    constructor(opts: IOptions) {
+        this.opts = opts;
+    };
+    start(): void {
+        this.opts.process.forEach(detail => {
+            console.log(`Starting ${detail.processName} [{${detail.processArgs}]...`);
+            const process: IProcess = this.createProcess(detail);
+            this.processList[detail.processName] = process;
+        });
+    }
+    stop(): void {
+        for (const key in this.processList) {
+            console.log(`Killing ...`);
+            this.processList[key].process.kill();
+            delete this.processList[key];
+        }
+    }
+    postBundle(context: any): void {
+        this.stop();
+        this.start();
+    }
+    createProcess(detail: IProcessDetail): IProcess {
+        const process: IProcess = {
+            process: spawn(detail.processName, detail.processArgs),
+            detail
+        };
+        if (detail.verbose) {
+            process.process.stdout.on('data', (data: BufferSource) => {
+                console.log(data.toString());
+            });
+            process.process.stderr.on('data', (data: BufferSource) => {
+                console.log(data.toString());
+            });
+        }
+        process.process.on('close', (code: any, signal: any) => {
+            console.log(`Finished ${detail.processName} [{${detail.processArgs}]...`);
+            delete this.processList[detail.processName];
+        });
+        return process;
+    }
+}
+
+export function ProcessPlugin(options: IOptions): ProcessPluginClass {
+    return new ProcessPluginClass(options);
+};
+
